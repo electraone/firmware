8c66b746bea8c1370090bc33817914b65fe692ce
Martin Pavlas <martin@think-forth.com>
2022-05-19 21:55:33 +0200

    Add the BottomBar component

---
+++ src/Components/BottomBar.cpp
@@ -0,0 +1,40 @@
+#include "BottomBar.h"
+#include "helpers.h"
+
+BottomBar::BottomBar(const char *newPresetName, const char *newPageName)
+{
+    setName("bottomBar");
+    setPresetName(newPresetName);
+    setPageName(newPageName);
+}
+
+void BottomBar::paint(Graphics &g)
+{
+    g.fillAll(Colours::black);
+    g.setColour(0x2104);
+    g.drawLine(8, 0, 1016, 0);
+    g.printText(
+        0, 4, presetName, TextStyle::smallWhiteOnBlack, 508, TextAlign::right);
+    g.printText(
+        520, 4, pageName, TextStyle::smallWhiteOnBlack, 508, TextAlign::left);
+    g.setColour(Colours::white);
+    g.fillRect(512, 9, 2, 2);
+    g.dim(0, 3, 1024, 13, Colours::black);
+}
+
+void BottomBar::setPresetName(const char *newName)
+{
+    copyString(presetName, newName, maxPresetNameLength);
+}
+
+void BottomBar::setPageName(const char *newName)
+{
+    copyString(pageName, newName, maxPageNameLength);
+}
+
+void BottomBar::update(const char *newPresetName, const char *newPageName)
+{
+    setPresetName(newPresetName);
+    setPageName(newPageName);
+    repaint();
+}
---
+++ src/Components/BottomBar.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Component.h"
+
+class BottomBar : public Component
+{
+private:
+    // \todo move to common config file
+    static const uint8_t maxPresetNameLength = 20;
+    static const uint8_t maxPageNameLength = 20;
+
+public:
+    BottomBar(const char *newPresetName, const char *newPageName);
+    virtual ~BottomBar() override = default;
+
+    void paint(Graphics &g);
+    void update(const char *newPresetName, const char *newPageName);
+
+private:
+    void setPresetName(const char *newName);
+    void setPageName(const char *newName);
+
+    char presetName[maxPresetNameLength + 1];
+    char pageName[maxPageNameLength + 1];
+};
