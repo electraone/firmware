4ceb49e81f2d9a6ced04c1c709fe66f5038c7d5e
Martin Pavlas <martin@think-forth.com>
2022-05-28 22:57:52 +0200

    Add support for lowercase characters

--- src/Graphics/Text.cpp
+++ src/Graphics/Text.cpp
@@ -6,7 +6,10 @@ static const uint16_t lookupSmall[] = {
     90,  95,  101, 110, 115, 123, 132, 139, 148, 156, 164, 172, 180,
     185, 189, 197, 205, 214, 221, 234, 244, 252, 262, 271, 279, 286,
     296, 305, 309, 315, 323, 331, 342, 351, 362, 370, 381, 389, 398,
-    407, 416, 426, 440, 450, 459, 468, 475, 482, 488, 496
+    407, 416, 426, 440, 450, 459, 468, 475, 482, 488, 496,
+    510, 519, 527, 535, 544, 553, 561, 570, 578, 583, 589, 596, 601, // a - m
+    613, 621, 630, 638, 647, 653, 661, 667, 676, 685, 697, 706, 715, // n - z
+    723, 729, 733, 740 // {|}~
 };
 
 static const uint16_t lookupMedium[] = {
@@ -14,7 +17,10 @@ static const uint16_t lookupMedium[] = {
     101, 105, 112, 121, 127, 137, 146, 156, 165, 174, 183, 192, 202,
     205, 209, 219, 228, 237, 245, 259, 271, 280, 291, 302, 310, 318,
     329, 339, 342, 348, 358, 366, 379, 390, 402, 411, 423, 433, 442,
-    452, 463, 474, 490, 501, 511, 521, 526, 532, 539, 549
+    452, 463, 474, 490, 501, 511, 521, 526, 532, 539, 549,
+    562, 571, 580, 588, 597, 606, 613, 623, 632, 636, 641, 649, 653, // a - m
+    666, 675, 685, 694, 703, 710, 718, 725, 734, 743, 757, 766, 776, // n - z
+    784, 791, 794, 800 // {|}~
 };
 
 static const uint16_t lookupLarge[] = {
@@ -22,7 +28,10 @@ static const uint16_t lookupLarge[] = {
     114, 118, 126, 137, 143, 154, 164, 175, 185, 196, 206, 217, 227,
     231, 236, 246, 256, 267, 275, 292, 305, 316, 327, 339, 348, 358,
     370, 382, 385, 392, 403, 412, 427, 439, 452, 462, 476, 487, 497,
-    509, 521, 533, 551, 564, 575, 586, 592, 600, 606, 617
+    509, 521, 533, 551, 564, 575, 586, 592, 600, 606, 617,
+    633, 642, 653, 661, 672, 682, 691, 701, 711, 715, 721, 731, 734, // a - m
+    750, 760, 771, 781, 791, 799, 808, 815, 825, 836, 852, 862, 873, // n - z
+    882, 890, 893, 900 // {|}~
 };
 
 static const uint8_t charWidthSmall[] = { 3,  4, 8, 6, 10, 9, 2, 4, 4, 6,  7,
@@ -54,7 +63,38 @@ static const uint8_t charWidthSmall[] = { 3,  4, 8, 6, 10, 9, 2, 4, 4, 6,  7,
                                           7, // X
                                           7, // Y
                                           7, // Z
-                                          3,  5, 3, 7, 7 };
+                                          3,  5, 3, 7, 7,
+                                          6, // a
+                                          5, // b,
+                                          5, // c,
+                                          7, // d
+                                          6, // e
+                                          4, // f
+                                          7, // g
+                                          6, // h
+                                          2, // i
+                                          4, // j
+                                          5, // k
+                                          3, // l
+                                          10, // m
+                                          6, // n
+                                          7, // o
+                                          5, // p
+                                          7, // q
+                                          3, // r
+                                          6, // s
+                                          4, // t
+                                          7, // u
+                                          6, // v
+                                          9, // w
+                                          6, // x
+                                          6, // y
+                                          6, // z
+                                          5, // {
+                                          2, // |
+                                          5, // }
+                                          7 // ~
+                                          };
 
 static const uint8_t charWidthMedium[] = { 3,  5, 10, 8, 12, 12, 3, 4, 4, 9,  9,
                                            3,  4, 3,  6, 8,  5,  8, 8, 9, 8,  8,
@@ -85,7 +125,38 @@ static const uint8_t charWidthMedium[] = { 3,  5, 10, 8, 12, 12, 3, 4, 4, 9,  9,
                                            10, // X
                                            10, // Y
                                            9, // Z
-                                           4,  7, 5,  8, 9 };
+                                           4,  7, 5,  8, 9,
+                                           8, // a
+                                           8, // b
+                                           7, // c
+                                           8, // d
+                                           7, // e
+                                           6, // f
+                                           9, // g
+                                           9, // h
+                                           3, // i
+                                           4, // j
+                                           7, // k
+                                           3, // l
+                                           13, // m
+                                           8, // n
+                                           9, // o
+                                           8, // p
+                                           8, // q
+                                           6, // r
+                                           7, // s
+                                           6, // t
+                                           8, // u
+                                           7, // v
+                                           13, // w
+                                           8, // x
+                                           9, // y
+                                           7, // z
+                                           6, // {
+                                           2, // |
+                                           5, // }
+                                           8  // ~
+                                       };
 
 static const uint8_t charWidthLarge[] = { 3,  5,  12, 9,  14, 13, 3, 4,
                                           4,  9,  9,  4,  5,  3,  7, 10,
@@ -117,7 +188,38 @@ static const uint8_t charWidthLarge[] = { 3,  5,  12, 9,  14, 13, 3, 4,
                                           11, // X
                                           10, // Y
                                           10, // Z
-                                          5,  7,  5,  10, 9 };
+                                          5,  7,  5,  10, 9,
+                                          8, // a
+                                          9, // b
+                                          7, // c
+                                          10, // d
+                                          8, // e
+                                          7, // f
+                                          9, // g
+                                          9, // h
+                                          3, // i
+                                          5, // j
+                                          8, // k
+                                          2, // l
+                                          15, // m
+                                          9, // n
+                                          9, // o
+                                          9, // p
+                                          9, // q
+                                          7, // r
+                                          7, // s
+                                          6, // t
+                                          9, // u
+                                          9, // v
+                                          14, // w
+                                          8, // x
+                                          10, // y
+                                          8, // z
+                                          7, // {
+                                          2, // |
+                                          6, // }
+                                          10 // ~
+                                      };
 
 Text::Text(const uint8_t CSp, const uint8_t RSTp) : FrameBuffer(CSp, RSTp)
 {
@@ -167,9 +269,9 @@ void Text::printText(uint16_t x,
             uint16_t charHeight = getCharPxHeight(textStyle);
             uint16_t charYposition = getCharPositionY(textStyle);
 
-            if (charHeight == 12) {
+            if (charHeight == 13) {
                 charXposition = lookupSmall[index];
-            } else if (charHeight == 16) {
+            } else if (charHeight == 17) {
                 charXposition = lookupMedium[index];
             } else {
                 charXposition = lookupLarge[index];
@@ -244,13 +346,16 @@ int8_t Text::getCharacterTableIndex(char c)
 {
     int8_t index = -1;
 
+
     if ((33 <= c) && (c <= 96)) {
         index = c - 33;
     } else if ((96 < c) && (c <= 127)) {
-        index = c - 65;
+        index = c - 34;
     } else {
         index = -1; // \todo unknown or space. this could be done better
     }
+
+
     return (index);
 }
 
@@ -304,15 +409,15 @@ uint16_t Text::getCharPxHeight(TextStyle textStyle)
 {
     if ((textStyle == TextStyle::smallWhiteOnBlack)
         || (textStyle == TextStyle::smallTransparent)) {
-        return (12);
+        return (13);
     } else if ((textStyle == TextStyle::largeWhiteOnBlack)
                || (textStyle == TextStyle::largeTransparent)) {
-        return (18);
+        return (19);
     } else if ((textStyle == TextStyle::mediumWhiteOnBlack)
                || (textStyle == TextStyle::mediumBlackOnWhite)
                || (textStyle == TextStyle::mediumWhiteOnDimmed)
                || (textStyle == TextStyle::mediumTransparent)) {
-        return (16);
+        return (17);
     }
 
     return (0);
