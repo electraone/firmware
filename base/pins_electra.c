c63b29487beaa27accac9c818c6f5b3ea7d41cce
Martin Pavlas <martin@think-forth.com>
2022-06-11 09:54:33 +0200

    Remove unneeded usb initialization code

--- base/pins_electra.c
+++ base/pins_electra.c
@@ -583,20 +583,6 @@ void _init_Teensyduino_internal_(void)
 	TPM1_SC = FTM_SC_CLKS(1) | FTM_SC_PS(0);
 #endif
 	analog_init();
-	// for background about this startup delay, please see these conversations
-	// https://forum.pjrc.com/threads/36606-startup-time-(400ms)?p=113980&viewfull=1#post113980
-	// https://forum.pjrc.com/threads/31290-Teensey-3-2-Teensey-Loader-1-24-Issues?p=87273&viewfull=1#post87273
-#if 0
-#if TEENSYDUINO >= 142
-	delay(25);
-	usb_init();
-	delay(275);
-#else
-	delay(50);
-	usb_init();
-	delay(350);
-#endif
-#endif
 }
 
 
