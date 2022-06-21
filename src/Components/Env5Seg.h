b1864a33fcdef75b2f3a716523e2f221b432fc7f
Martin Pavlas <martin@think-forth.com>
2022-06-21 07:17:42 +0200

    Swap levels and rates for 5 segment envelope

--- src/Components/Env5Seg.h
+++ src/Components/Env5Seg.h
@@ -13,14 +13,14 @@ public:
         points.push_back(Point()); // Stage 4
         points.push_back(Point()); // Stage 5
 
-        values.push_back(Value()); // Rate 1
         values.push_back(Value()); // Level 1
-        values.push_back(Value()); // Rate 2
+        values.push_back(Value()); // Rate 1
         values.push_back(Value()); // Level 2
-        values.push_back(Value()); // Rate 3
+        values.push_back(Value()); // Rate 2
         values.push_back(Value()); // Level 3
-        values.push_back(Value()); // Rate 4
+        values.push_back(Value()); // Rate 3
         values.push_back(Value()); // Level 4
+        values.push_back(Value()); // Rate 4
     }
 
     virtual ~Env5Seg() = default;
@@ -101,14 +101,14 @@ public:
         baselineY = baseline;
     }
 
-    static constexpr uint8_t rate1 = 0;
-    static constexpr uint8_t level1 = 1;
-    static constexpr uint8_t rate2 = 2;
-    static constexpr uint8_t level2 = 3;
-    static constexpr uint8_t rate3 = 4;
-    static constexpr uint8_t level3 = 5;
-    static constexpr uint8_t rate4 = 6;
-    static constexpr uint8_t level4 = 7;
+    static constexpr uint8_t level1 = 0;
+    static constexpr uint8_t rate1 = 1;
+    static constexpr uint8_t level2 = 2;
+    static constexpr uint8_t rate2 = 3;
+    static constexpr uint8_t level3 = 4;
+    static constexpr uint8_t rate3 = 5;
+    static constexpr uint8_t level4 = 6;
+    static constexpr uint8_t rate4 = 7;
 
 private:
 };
