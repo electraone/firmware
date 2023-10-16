b78dd2e4e7281d1199feb351d7ba8b565a641366
Martin Pavlas <martin@electra.one>
2023-10-16 20:46:54 +0200

    Add header comments

--- src/Memory/BitmapPool.cpp
+++ src/Memory/BitmapPool.cpp
@@ -1,3 +1,30 @@
+/*
+* Electra One MIDI Controller Firmware
+* See COPYRIGHT file at the top of the source tree.
+*
+* This product includes software developed by the
+* Electra One Project (http://electra.one/).
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.
+*/
+
+/**
+ * @file BitmapPool.cpp
+ *
+ * @brief A representation of the E1 style memory pool to hold Bitmap data.
+ */
+
 #include "BitmapPool.h"
 #include "Colours.h"
 #include "Graphics.h"
