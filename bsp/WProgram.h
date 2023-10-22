9cbdfcbd6372706e99db552822b0ed4b50bbf4c3
Martin Pavlas <martin@electra.one>
2023-10-22 16:18:10 +0200

    Remove Wcharacter class

--- bsp/WProgram.h
+++ bsp/WProgram.h
@@ -22,7 +22,6 @@
 #include "usb_mouse.h"
 #include "usb_undef.h" // do not allow usb_desc.h stuff to leak to user programs
 
-#include "WCharacter.h"
 #include "WString.h"
 #include "elapsedMillis.h"
 #include "IntervalTimer.h"
