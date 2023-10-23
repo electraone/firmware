ebb9e921c1eb393cba0218c580ab59fc9aaed6a2
Martin Pavlas <martin@electra.one>
2023-10-23 16:31:00 +0200

    Remove unneeded ifdefs

--- bsp/Random.h
+++ bsp/Random.h
@@ -1,16 +1,9 @@
-#ifndef RANDOM_H
-#define RANDOM_H
+#pragma once
 
 #include <stdint.h>
-
-#ifdef __cplusplus
 
 int32_t random(void);
 uint32_t random(uint32_t howbig);
 int32_t random(int32_t howsmall, int32_t howbig);
 void randomSeed(uint32_t newseed);
 void srandom(unsigned int newseed);
-
-#endif // __cplusplus
-
-#endif // RANDOM_H
