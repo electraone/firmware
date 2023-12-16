e481e9877b6d1ddbb2d56422e4eececd14404be4
Martin Pavlas <martin@electra.one>
2023-12-16 18:10:14 +0100

    Mark as version 3.5.3

--- src/settings.h
+++ src/settings.h
@@ -29,8 +29,8 @@
 
 #pragma once
 
-#define FIRMWARE_VERSION "v3.5.2"
-#define FIRMWARE_SEQ 300500200
+#define FIRMWARE_VERSION "v3.5.3"
+#define FIRMWARE_SEQ 300500300
 #define UI_FILE "ui-0.9.6.bmp"
 #define JSON_VERSION 2
 
